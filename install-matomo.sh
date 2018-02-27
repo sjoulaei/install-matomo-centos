4a564e18f19cc11b069102b7305af1a7d3c5f080
Saeed Joulaei <sjoulaei@users.noreply.github.com>
2018-02-27 21:59:52 +1100

    removed the copy from internet sections
    it is more generic now. Files need to be copied or generated outside of this script.

--- install-matomo.sh
+++ install-matomo.sh
@@ -40,12 +40,14 @@ sed -i 's/^SELINUX=.*/SELINUX=permissive/' /etc/sysconfig/selinux && echo SUCCES
 
 
 #copy your ssl certificates
-echo -e "For SSL certificates to work properly you need to copy the certificate files. I assume you have them already somewhere accessible on the net."
-read -p "Enter the source location for your ssl certificates (*.crt):" ssl_crt
-read -p "\033[32mEnter the source location for your ssl certificate key file (*.key):" ssl_key
+echo -e "For SSL certificates to work properly you need to copy the certificate files into the right location. I assume you have them in below addresses:"
+echo -e "certificate file: /etc/pki/tls/certs/your_cert_file.crt"
+echo -e "certificate key file: /etc/pki/tls/private/your_private_key_file.key"
 
-scp -v $ssl_crt /etc/pki/tls/certs/
-scp -v $ssl_key /etc/pki/tls/private/
+read -p "Enter the source location for your ssl certificates (localhost.crt):" ssl_crt
+ssl_crt=${ssl_crt:-"localhost.crt"}
+read -p "\033[32mEnter the source location for your ssl certificate key file (localhost.key):" ssl_key
+ssl_key=${ssl_key:-"localhost.key"}
 
 sed -i "s|SSLCertificateFile.*|SSLCertificateFile /etc/pki/tls/certs/$ssl_crt|" /opt/rh/httpd24/root/etc/httpd/conf.d/matomo.conf  && echo SUCCESS || echo FAILURE
 sed -i "s|SSLCertificateKeyFile.*|SSLCertificateKeyFile /etc/pki/tls/private/$ssl_key|" /opt/rh/httpd24/root/etc/httpd/conf.d/matomo.conf  && echo SUCCESS || echo FAILURE
